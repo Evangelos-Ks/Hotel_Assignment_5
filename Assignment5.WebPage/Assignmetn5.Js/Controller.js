9e43b8db5f912a15256030f374dc86afbdf504c7
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-05-17 10:07:08 +0100

    Pulled data in  the Contoller

--- Assignment5.WebPage/Assignmetn5.Js/Controller.js
+++ Assignment5.WebPage/Assignmetn5.Js/Controller.js
@@ -1,3 +1,40 @@
-﻿(function () {
-    var HottelApp = 
+﻿
+(function () {
+    var HotelApp = angular.module("HotelApp", []);
+
+    var url = "../data.json";
+
+
+    var HotelController = function ($scope, $http) {
+
+
+        function GetData() {
+            $http.get(url)
+                .then(function (response) {
+                    console.log(response); //Take and display data
+                })
+        };
+
+        GetData();//Execute GetData
+    };
+
+        HotelApp.controller("HotelController", HotelController);
 })();
+
+//app.controller('myCtrl', function ($scope, $http) {
+//    $http.get("welcome.htm")
+//        .then(function (response) {
+//            $scope.content = response.data;
+//            $scope.statuscode = response.status;
+//            $scope.statustext = response.statusText;
+//        });
+//});
+//function ($scope, $http) {
+//    $http.get("welcome.htm")
+//        .then(function (response) {
+//            $scope.content = response.data;
+//            $scope.statuscode = response.status;
+//            $scope.statustext = response.statusText;
+//        });
+//});
+
