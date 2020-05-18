39830a4caffbad0bb39d7cf49fc13d57eb5400df
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-05-18 13:09:59 +0100

    Slide bar ready

--- Assignment5.WebPage/Assignmetn5.Js/Controller.js
+++ Assignment5.WebPage/Assignmetn5.Js/Controller.js
@@ -12,30 +12,64 @@
         function GetData() {
             $http.get(url)
                 .then(function (response) {
-                    var hotels = [];
+
+                    //MaxPrice
+                    function MaxPrice(Hotels) {
+                        var price = 0;
+
+                        for (var i in Hotels) {
+                            if (Hotels[i].price > price) {
+                                price = Hotels[i].price;
+                            }
+                        }
+                        return price;
+                    }
+
+
                     $scope.Roomtypes = response.data[0].roomtypes; //roomtypes
                     $scope.Hotels = response.data[1].entries; //Hotels
+                    $scope.MaxPrice = MaxPrice(response.data[1].entries);//max price
+                    $scope.MaxPrice2 = MaxPrice(response.data[1].entries);//max price
 
+
+                    //autocomplete
                     autocomplete(document.getElementById("search"), AutocompleteItems(response.data[1].entries));
 
+
+                    //Submit btn
                     document.getElementById("submitBtn").addEventListener("click", function () {
                         var text = document.getElementById("search").value;
+                        var hotels = [];
+
                         for (var i in response.data[1].entries) {
 
                             if (text == response.data[1].entries[i].city) {
                                 hotels.push(response.data[1].entries[i]);
                             }
                         }
-                        console.log(hotels);
-                        $scope.Hotels = hotels;
-                        $scope.$apply();
-                        hotels = [];
+                        if (hotels.length >= 1) {
+                            console.log(hotels);
+                            $scope.Hotels = hotels;
+                            $scope.MaxPrice = MaxPrice(hotels);
+                            $scope.MaxPrice2 = MaxPrice(hotels);
+                            $scope.$apply();
+                            hotels = [];
+                        }
+                        else {
+                            $scope.Hotels = response.data[1].entries; 
+                            $scope.MaxPrice = MaxPrice(response.data[1].entries);
+                            $scope.MaxPrice2 = MaxPrice(response.data[1].entries);
+                            $scope.$apply();
+                            hotels = [];
+                        }
+                        
                     });
 
-                   
+                    $scope.filterPrice = function (obj) {
+                        return obj.price > 0 && obj.price <= $scope.MaxPrice;
+                    };
 
-                    console.log($scope.Hotels);
-                })
+                });
 
         };
 
@@ -44,6 +78,7 @@
 
     HotelApp.controller("HotelController", HotelController);
 })();
+
 
 
 
